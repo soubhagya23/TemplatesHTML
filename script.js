e7da01675e180cbb5aa734e6b776f90992686252
soubhagya23 <ranjansoubhagya.master@gmail.com>
2021-09-12 10:32:25 +0530

    Experiment

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let name1 = prompt("Enter Groom Name: ");
 let name2 = prompt("Enter Bride Name: ");
-
-
-
-
 let groom = document.getElementById("groom");
 let bride = document.getElementById("bride");
-
-
 groom.innerHTML = `${name1}`;
 bride.innerHTML = `${name2}`;
